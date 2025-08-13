671bada9799ec58370e4a87b496aa9d6d69ebc16
Alex Lesnoy <123098237+bebelekele@users.noreply.github.com>
2025-08-13 17:30:20 +0300

    Adding solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,85 @@
-export interface Figure {}
+export interface Figure {
+  a: number;
+  shape: string;
+  color: string;
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  public shape: string = 'triangle';
 
-export class Circle implements Figure {}
+  public square: number;
 
-export class Rectangle implements Figure {}
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
 
-export function getInfo(figure): string {
-  return typeof figure;
+    return Math.floor(area * 100) / 100;
+  }
+
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    this.square = this.getArea();
+
+    if (this.square <= 0) {
+      throw new Error('your message');
+    }
+  }
+}
+
+export class Circle implements Figure {
+  public square: number;
+
+  public shape: string = 'circle';
+
+  getArea(): number {
+    const area = Math.PI * this.a ** 2;
+
+    return Math.floor(area * 100) / 100;
+  }
+
+  constructor(
+    public color: string,
+    public a: number,
+  ) {
+    if (this.a <= 0) {
+      throw new Error('your message');
+    }
+
+    this.square = this.getArea();
+
+    if (this.square <= 0) {
+      throw new Error('your message');
+    }
+  }
+}
+
+export class Rectangle implements Figure {
+  public square: number;
+
+  public shape: string = 'rectangle';
+
+  getArea(): number {
+    return this.a * this.b;
+  }
+
+  constructor(
+    public color: string,
+    public a: number,
+    public b: number,
+  ) {
+    this.square = this.getArea();
+
+    if (this.square <= 0) {
+      throw new Error('your message');
+    }
+  }
+}
+
+export function getInfo(figure: Triangle | Rectangle | Circle): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.square}`;
 }
